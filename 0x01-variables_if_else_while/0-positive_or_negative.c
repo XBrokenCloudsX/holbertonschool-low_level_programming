39391a316caa619b08484a636bebb4dac45b9866
XBrokenCloudsX <2395@holbertonschool.com>
2020-09-17 18:59:34 +0000

    Commands

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *main - prints a number and wehter the last digit is greater than 5, less than
+ * 6 or zero
+ *
+ *Return: 0 always zero
+ */
 int main(void)
 {
   int n;
@@ -10,5 +14,18 @@ int main(void)
   srand(time(0));
   n = rand() - RAND_MAX / 2;
   /* your code goes there */
+
+  int d= n % 10;
+
+  printf("Last digit of %l and ls ", n , d);
+  if (d > 5)
+    puts("greater than 5");
+  
+  else if (d == 0)
+    puts ("0");
+  
+  else
+    puts("less than 6 and not 0")
+  
   return (0);
 }
