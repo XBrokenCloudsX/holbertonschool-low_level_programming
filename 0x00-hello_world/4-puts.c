d04dc3e2a2e280ffb2ac0b63f8d03c5456c89e19
XBrokenCloudsX <2395@holbertonschool.com>
2020-09-21 15:32:47 +0000

    Commands

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
+/**
+* main - Print quote
+*
+* Return: return 0 value at the end
+*/
 int main(void)
 {
- puts("Programming is like building a multilingual puzzle\n");
+ puts("\"Programming is like building a multilingual puzzle\n");
  return 0;
 }
